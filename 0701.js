23d36768ce6e3d80fabd6a7e3f6433fc69dcf49e
Javier Pérez <javier.perez@ufirstgroup.ch>
2021-12-07 06:57:57 +0100

    star 7

---
+++ 0701.js
@@ -0,0 +1,31 @@
+const readfile = require('./readfile')
+
+let positions
+;(async () => {
+  await readfile('0701.txt', (line) => {
+    positions = line.split(',').map((val) => parseInt(val))
+  })
+
+  let min, max
+  positions.forEach((val) => {
+    min = min === undefined ? val : Math.min(min, val)
+    max = max === undefined ? val : Math.max(max, val)
+  })
+
+  let minFuel = max * positions.length * 1000
+  for (let position = min; position <= max; position++) {
+    const fuel = positions
+      .map((val) => {
+        const diff = Math.abs(val - position)
+        let fuel = 0
+        for (let inc = 1; inc <= diff; inc++) {
+          fuel += inc
+        }
+        return fuel
+      })
+      .reduce((acc, val) => acc + val)
+    minFuel = Math.min(minFuel, fuel)
+  }
+
+  console.log('result:', minFuel)
+})()
